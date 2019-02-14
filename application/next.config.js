ed48590cb476e525b7af8ba55d0964c952c1110d
Giuseppe Menti <mentifg@gmail.com>
2019-02-14 17:30:06 -0200

    Add default structure

---
+++ application/next.config.js
@@ -0,0 +1,10 @@
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const config = {
+  distDir: '../functions/.next',
+  assetPrefix: 'http://localhost:5000/hotelaria-f8835/us-central1/application/',
+};
+
+module.exports = config;
